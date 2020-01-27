1d80e175f1d6226123d1a6a6495689c8e4c36c67
Naqqash Khan <naqqash10@hotmail.com>
2020-01-27 20:18:10 +0500

    Partially Added Google Maps

--- FoodOrderingTask/DashBoard/frm_Vicinity.cs
+++ FoodOrderingTask/DashBoard/frm_Vicinity.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GMap.NET;
+using GMap.NET.MapProviders;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,57 +21,67 @@ namespace FoodOrderingTask.DashBoard
             string city = txt_city.Text;
             string state = txt_state.Text;
             string zip = txt_zip.Text;
-            try
-            {
-                StringBuilder queryaddress = new StringBuilder();
-                queryaddress.Append("https://www.google.com/maps");
+        //    try
+        //    {
+        //        StringBuilder queryaddress = new StringBuilder();
+        //        queryaddress.Append("https://www.google.com/maps");
 
-                if (street != string.Empty)
-                {
-                    queryaddress.Append("Clementi, Singapore");
-                }
+        //        if (street != string.Empty)
+        //        {
+        //            queryaddress.Append("Clementi, Singapore");
+        //        }
 
-                webBrowser1.Navigate(queryaddress.ToString());
+        //        webBrowser1.Navigate(queryaddress.ToString());
 
 
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message.ToString(), "Error");
-            }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show(ex.Message.ToString(), "Error");
+        //    }
 
         }
 
         private void frm_Vicinity_Load(object sender, EventArgs e)
         {
-
+            map.DragButton = MouseButtons.Left;
+            map.MapProvider = GMapProviders.GoogleMap;
+            map.MinZoom = 0;
+            map.MaxZoom = 18;
+            map.Zoom = 5;
+          
         }
 
         private void Search_Data_Click(object sender, EventArgs e)
         {
-            {
+            //{
 
-                string zip = txt_zip.Text;
-                try
-                {
-                    StringBuilder queryaddress = new StringBuilder();
-                    queryaddress.Append("https://www.google.com/maps");
+        //        string zip = txt_zip.Text;
+        //        try
+        //        {
+        //            StringBuilder queryaddress = new StringBuilder();
+        //            queryaddress.Append("https://www.google.com/maps");
 
-                    if (zip != string.Empty)
-                    {
-                        queryaddress.Append(zip);
-                    }
+        //            if (zip != string.Empty)
+        //            {
+        //                queryaddress.Append(zip);
+        //            }
 
-                    webBrowser1.Navigate(queryaddress.ToString());
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(ex.Message.ToString(), "Error");
-                }
-            }
+        //            webBrowser1.Navigate(queryaddress.ToString());
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            MessageBox.Show(ex.Message.ToString(), "Error");
+        //        }
+        //    }
         }
 
         private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void gMapControl_Load(object sender, EventArgs e)
         {
 
         }
